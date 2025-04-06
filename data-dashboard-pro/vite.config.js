4cf314fc35bff9af11bb80460bf5a6b1049e9d29
C0cuy0 <am.private.g@gmail.com>
2025-04-05 18:50:41 -0600

    : /

--- data-dashboard-pro/vite.config.js
+++ data-dashboard-pro/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/brewery-data-dashboard-pro/data-dashboard-pro/', // Add this line
+  base: '/brewery-data-dashboard-pro/', // Add this line
   plugins: [react()],
 })
