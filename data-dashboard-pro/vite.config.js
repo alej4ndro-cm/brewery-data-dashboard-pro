c17f9326da1d5b19b85a93d8dd86c23dd4e361da
C0cuy0 <am.private.g@gmail.com>
2025-04-13 13:15:10 -0600

    configuring vite.js file

--- data-dashboard-pro/vite.config.js
+++ data-dashboard-pro/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/brewery-data-dashboard-pro/', // Corrected for GitHub Pages deployment
+  plugins: [react()]
 })
